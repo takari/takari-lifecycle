e8ec11163d1bc0ec983460d71f2f554341952460
Jason van Zyl <jason@tesla.io>
2013-11-23 10:55:31 -0500

    Add test for support of the targetPath option for resource processing

--- tesla-lifecycle-plugin/src/test/java/io/tesla/maven/plugins/resources/ResourcesBuildAvoidanceTest.java
+++ tesla-lifecycle-plugin/src/test/java/io/tesla/maven/plugins/resources/ResourcesBuildAvoidanceTest.java
@@ -9,7 +9,7 @@ import com.google.common.io.Files;
 
 public class ResourcesBuildAvoidanceTest extends AbstractBuildAvoidanceTest {
   
-  public void testResourcesMojoWithNoFiltering() throws Exception {
+  public void testResources() throws Exception {
     File basedir = getBasedir("src/test/projects/project-with-resources");
     executeMojo(basedir, "process-resources");
     File resource = new File(basedir, "target/classes/resource.txt");
@@ -17,8 +17,17 @@ public class ResourcesBuildAvoidanceTest extends AbstractBuildAvoidanceTest {
     String line = Files.readFirstLine(resource, Charset.defaultCharset());
     assertTrue(line.contains("resource.txt"));
   }
-  
-  public void testResourcesMojoWithFiltering() throws Exception {
+
+  public void testResourcesWithTargetPath() throws Exception {
+    File basedir = getBasedir("src/test/projects/project-with-resources-with-target-path");
+    executeMojo(basedir, "process-resources");
+    File resource = new File(basedir, "target/classes/resources/targetPath/resource.txt");
+    assertTrue(resource.exists());
+    String line = Files.readFirstLine(resource, Charset.defaultCharset());
+    assertTrue(line.contains("resource.txt"));
+  }
+
+  public void testResourcesWithFiltering() throws Exception {
     File basedir = getBasedir("src/test/projects/project-with-resources-filtered");
     executeMojo(basedir, "process-resources");
     assertTrue(new File(basedir, "target/classes/resource.txt").exists());
