9e54788b5701a0795830f06db807cdb84571faba
Jason van Zyl <jason@tesla.io>
2013-11-23 11:51:50 -0500

    Create a resource processor JSR330 component that can deal with resource copying/filtering generally I need to make the process-test-resources goal, but one deficiency with the current plugin descriptor generation process is that I need to create another mojo, and generally we have usually made an abstract base and then the application process and test process subclass this so that there are two mojo descriptors generated. It would be nicer if you just had your one mojo and could create two descriptors which used the same mojo with different parameters. Better yet it would be nice if the JSR330 component could be used and configured and run in the Maven build.

--- tesla-lifecycle-plugin/src/main/java/io/tesla/maven/plugins/resources/Resources.java
+++ tesla-lifecycle-plugin/src/main/java/io/tesla/maven/plugins/resources/Resources.java
@@ -3,11 +3,7 @@ package io.tesla.maven.plugins.resources;
 import io.tesla.maven.plugins.TeslaLifecycleMojo;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
 import java.util.List;
 import java.util.Properties;
 
@@ -15,15 +11,9 @@ import javax.inject.Inject;
 
 import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.eclipse.tesla.incremental.BuildContext;
-import org.eclipse.tesla.incremental.DefaultFileSet;
-import org.eclipse.tesla.incremental.FileSetBuilder;
 import org.sonatype.maven.plugin.Conf;
 import org.sonatype.maven.plugin.LifecycleGoal;
 import org.sonatype.maven.plugin.LifecyclePhase;
-
-import com.google.common.io.CharStreams;
-import com.google.common.io.Closer;
 
 @LifecycleGoal(goal = "process-resources", phase = LifecyclePhase.PROCESS_RESOURCES)
 public class Resources extends TeslaLifecycleMojo {
@@ -38,58 +28,28 @@ public class Resources extends TeslaLifecycleMojo {
   private Properties properties;
 
   @Inject
-  private BuildContext buildContext;
-
-  private final ResourceFilter resourceFilter;
-
-  public Resources() {
-    resourceFilter = new ResourceFilter();
-  }
-
+  private ResourcesProcessor processor;
+  
   @Override
   protected void executeMojo() throws MojoExecutionException {
-    List<Resource> resourceDirectories = project.getBuild().getResources();
-    for (Resource resource : resourceDirectories) {
-      boolean filter = Boolean.parseBoolean(resource.getFiltering());
-      // Resource directories are already pre-pended with ${project.basedir}
-      File inputDir = new File(resource.getDirectory());
-      List<String> includes = resource.getIncludes();
-      if(includes.isEmpty()) {
-        includes.add("**/**");
-      }
-      DefaultFileSet fileSet = new FileSetBuilder(inputDir) //
-          .addIncludes(includes) //
-          .addExcludes(resource.getExcludes()) //
-          .build();
-      for (File inputFile : buildContext.registerInputs(fileSet)) {
-        buildContext.addProcessedInput(inputFile);
-        File outputFile;
-        if (resource.getTargetPath() != null) {
-          // A custom location within the outputDirectory, the targetPath is relative to the outputDirectory
-          outputFile = fileSet.relativize(new File(outputDirectory, resource.getTargetPath()), inputFile);
-        } else {
-          // Use the default output directory
-          outputFile = fileSet.relativize(outputDirectory, inputFile);
-        }
-        Closer closer = Closer.create();
-        try {
-          try {
-            Reader reader = closer.register(new FileReader(inputFile));
-            Writer writer = closer.register(new OutputStreamWriter(buildContext.newOutputStream(inputFile, outputFile)));
-            if (filter) {
-              resourceFilter.filter(reader, writer, properties);
-            } else {
-              CharStreams.copy(reader, writer);
-            }
-          } catch (IOException e) {
-            throw new MojoExecutionException(e.getMessage(), e);
-          } finally {
-            closer.close();
-          }
-        } catch (IOException e) {
-          throw new MojoExecutionException(e.getMessage(), e);
-        }
-      }
-    }
+    process(project.getBuild().getResources());
   }
+
+  protected void process(List<Resource> resources) throws MojoExecutionException {
+    for (Resource resource : resources) {
+      boolean filter = Boolean.parseBoolean(resource.getFiltering());
+      File inputDir = new File(resource.getDirectory());
+      File outputDir;
+      if (resource.getTargetPath() != null) {
+        outputDir = new File(outputDirectory, resource.getTargetPath());
+      } else {
+        outputDir = outputDirectory;
+      }
+      try {
+        processor.process(inputDir, outputDir, resource.getIncludes(), resource.getExcludes(), filter, properties);
+      } catch (IOException e) {
+        throw new MojoExecutionException(e.getMessage(), e);
+      }
+    }    
+  }  
 }
---
+++ tesla-lifecycle-plugin/src/main/java/io/tesla/maven/plugins/resources/ResourcesProcessor.java
@@ -0,0 +1,85 @@
+package io.tesla.maven.plugins.resources;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.eclipse.tesla.incremental.BuildContext;
+import org.eclipse.tesla.incremental.DefaultFileSet;
+import org.eclipse.tesla.incremental.FileSetBuilder;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheException;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Closer;
+
+@Named
+@Singleton
+public class ResourcesProcessor {
+  
+  private final BuildContext buildContext;
+  private final DefaultMustacheFactory mustacheFactory;  
+  
+  @Inject
+  public ResourcesProcessor(BuildContext buildContext) {
+    this.buildContext = buildContext;
+    this.mustacheFactory = new NoEncodingMustacheFactory();
+  }
+  
+  //TODO: don't need boolean flag and properties for filtering
+  protected void process(File sourceDirectory, File targetDirectory, List<String> includes, List<String> excludes, boolean filter, Properties filterProperties) throws IOException {
+    if (includes.isEmpty()) {
+      includes.add("**/**");
+    }
+    DefaultFileSet fileSet = new FileSetBuilder(sourceDirectory) //
+        .addIncludes(includes) //
+        .addExcludes(excludes) //
+        .build();
+    for (File inputFile : buildContext.registerInputs(fileSet)) {
+      buildContext.addProcessedInput(inputFile);
+      File outputFile = fileSet.relativize(targetDirectory, inputFile);
+      Closer closer = Closer.create();
+      try {
+        Reader reader = closer.register(new FileReader(inputFile));
+        Writer writer = closer.register(new OutputStreamWriter(buildContext.newOutputStream(inputFile, outputFile)));
+        if (filter) {
+          filter(reader, writer, filterProperties);
+        } else {
+          CharStreams.copy(reader, writer);
+        }
+      } finally {
+        closer.close();
+      }
+    }
+  }
+  
+  public void filter(Reader reader, Writer writer, Map<Object,Object> properties) throws IOException {
+    Mustache mustache = mustacheFactory.compile(reader, "maven", "${", "}");
+    mustache.execute(writer, properties).close();
+  }
+
+  private static class NoEncodingMustacheFactory extends DefaultMustacheFactory {
+    @Override
+    public void encode(String value, Writer writer) {
+      //
+      // TODO: encoding rules
+      //
+      try {
+        writer.write(value);
+      } catch (IOException e) {
+        throw new MustacheException(e);
+      }
+    }
+  }
+}
