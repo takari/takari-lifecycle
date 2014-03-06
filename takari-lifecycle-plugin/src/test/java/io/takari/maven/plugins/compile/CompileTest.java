40598d4fb5e3e95b1f5273c4bccc32ac845665f6
Igor Fedorenko <igor@ifedorenko.com>
2014-03-06 11:50:04 -0500

    introduced AbstractCompileTest
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- takari-lifecycle-plugin/src/test/java/io/takari/maven/plugins/compile/CompileTest.java
+++ takari-lifecycle-plugin/src/test/java/io/takari/maven/plugins/compile/CompileTest.java
@@ -1,112 +1,20 @@
 package io.takari.maven.plugins.compile;
 
-import static org.apache.maven.plugin.testing.resources.TestResources.cp;
-import io.takari.incrementalbuild.maven.testing.IncrementalBuildRule;
 import io.takari.maven.plugins.compiler.incremental.AbstractCompileMojo.Proc;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Set;
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DefaultArtifact;
-import org.apache.maven.artifact.handler.ArtifactHandler;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.testing.resources.TestResources;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.junit.Assert;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(Parameterized.class)
-public class CompileTest {
-
-  @Rule
-  public final TestResources resources = new TestResources();
-
-  @Rule
-  public final IncrementalBuildRule mojos = new IncrementalBuildRule();
-
-  private final String compilerId;
-
-  private final boolean fork;
+public class CompileTest extends AbstractCompileTest {
 
   public CompileTest(String compilerId, boolean fork) {
-    this.compilerId = compilerId;
-    this.fork = fork;
-  }
-
-  @Parameters
-  public static Iterable<Object[]> compilers() {
-    return Arrays.asList( //
-        new Object[] {"javac", Boolean.FALSE}, //
-        new Object[] {"javac", Boolean.TRUE} //
-        );
-  }
-
-  private File compile(String name) throws Exception {
-    File basedir = resources.getBasedir(name);
-    compile(basedir);
-    return basedir;
-  }
-
-  private void compile(File basedir) throws Exception {
-    MavenProject project = mojos.readMavenProject(basedir);
-    MavenSession session = mojos.newMavenSession(project);
-    MojoExecution execution = newMojoExecution();
-
-    mojos.executeMojo(session, project, execution);
-  }
-
-  private MojoExecution newMojoExecution() {
-    MojoExecution execution = mojos.newMojoExecution("compile-incremental");
-
-    Xpp3Dom configuration = execution.getConfiguration();
-    add(configuration, "fork", Boolean.toString(fork));
-    return execution;
-  }
-
-  private void addDependency(MavenProject project, String artifactId, File file) throws Exception {
-    ArtifactHandler handler = mojos.getContainer().lookup(ArtifactHandler.class, "jar");
-    DefaultArtifact artifact =
-        new DefaultArtifact("test", artifactId, "1.0", Artifact.SCOPE_COMPILE, "jar", null, handler);
-    artifact.setFile(file);
-    Set<Artifact> artifacts = project.getArtifacts();
-    artifacts.add(artifact);
-    project.setArtifacts(artifacts);
-  }
-
-  private void add(Xpp3Dom configuration, String name, String value) {
-    Xpp3Dom child = new Xpp3Dom(name);
-    child.setValue(value);
-    configuration.addChild(child);
-  }
-
-  private File procCompile(String projectName, Proc proc) throws Exception, IOException {
-    File processor = compile("compile/processor");
-    cp(processor, "src/main/resources/META-INF/services/javax.annotation.processing.Processor",
-        "target/classes/META-INF/services/javax.annotation.processing.Processor");
-
-    File basedir = resources.getBasedir(projectName);
-    MavenProject project = mojos.readMavenProject(basedir);
-    MavenSession session = mojos.newMavenSession(project);
-    MojoExecution execution = newMojoExecution();
-
-    addDependency(project, "processor", new File(processor, "target/classes"));
-
-    if (proc != null) {
-      add(execution.getConfiguration(), "proc", proc.name());
-    }
-
-    mojos.executeMojo(session, project, execution);
-    return basedir;
+    super(compilerId, fork);
   }
 
   @Test
