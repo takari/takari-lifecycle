22c709e632ae748495d1cc08bea42c5640d761c7
Igor Fedorenko <igor@ifedorenko.com>
2014-08-06 12:28:56 +0400

    fixed testProperties writes different output on each build
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

---
+++ takari-plugin-testing-plugin/src/main/java/io/takari/maven/plugins/plugin/testing/PropertiesWriter.java
@@ -0,0 +1,45 @@
+package io.takari.maven.plugins.plugin.testing;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.CharSource;
+import com.google.common.io.CharStreams;
+
+/**
+ * Helper to strip idiotic timestamp comment from properties files
+ */
+class PropertiesWriter {
+  // properties files are documented to use ISO_8859_1 encoding
+  private static final Charset ENCODING = Charsets.ISO_8859_1;
+
+  public static void write(Properties properties, String comment, OutputStream out) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    properties.store(CharStreams.asWriter(sb), comment);
+    write(CharStreams.asCharSource(sb.toString()), comment, out);
+  }
+
+  public static void write(byte[] properties, OutputStream out) throws IOException {
+    // properties files are documented to use ISO_8859_1 encoding
+    write(ByteStreams.asByteSource(properties).asCharSource(ENCODING), null, out);
+  }
+
+  private static void write(CharSource charSource, String comment, OutputStream out) throws IOException {
+    List<String> lines = new ArrayList<>(charSource.readLines());
+    lines.remove(comment != null ? 1 : 0);
+    BufferedWriter w = new BufferedWriter(new OutputStreamWriter(out, ENCODING));
+    for (String line : lines) {
+      w.write(line);
+      w.newLine();
+    }
+    w.flush();
+  }
+}
---
+++ takari-plugin-testing-plugin/src/test/java/io/takari/maven/plugins/plugin/testing/tests/TestPropertiesMojoTest.java
@@ -0,0 +1,111 @@
+package io.takari.maven.plugins.plugin.testing.tests;
+
+import io.takari.incrementalbuild.maven.testing.IncrementalBuildRule;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.execution.DefaultMavenExecutionRequest;
+import org.apache.maven.execution.DefaultMavenExecutionResult;
+import org.apache.maven.execution.MavenExecutionRequest;
+import org.apache.maven.execution.MavenExecutionRequestPopulator;
+import org.apache.maven.execution.MavenExecutionResult;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.execution.ProjectDependencyGraph;
+import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.MojoExecution;
+import org.apache.maven.plugin.testing.resources.TestResources;
+import org.apache.maven.plugin.testing.stubs.ArtifactStub;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
+import org.codehaus.plexus.util.ReflectionUtils;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.google.common.base.Throwables;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+
+public class TestPropertiesMojoTest {
+  @Rule
+  public final TestResources resources = new TestResources();
+
+  @Rule
+  public final IncrementalBuildRule mojos = new IncrementalBuildRule() {
+    @Override
+    public MavenSession newMavenSession(final MavenProject project) {
+      MavenExecutionRequest request = new DefaultMavenExecutionRequest();
+      try {
+        lookup(MavenExecutionRequestPopulator.class).populateDefaults(request);
+      } catch (Exception e) {
+        throw Throwables.propagate(e);
+      }
+      MavenExecutionResult result = new DefaultMavenExecutionResult();
+      MavenSession session = new MavenSession(getContainer(), MavenRepositorySystemUtils.newSession(), request, result);
+      session.setCurrentProject(project);
+      session.setProjects(Arrays.asList(project));
+      session.setProjectDependencyGraph(new ProjectDependencyGraph() {
+
+        @Override
+        public List<MavenProject> getSortedProjects() {
+          return Collections.singletonList(project);
+        }
+
+        @Override
+        public List<MavenProject> getDownstreamProjects(MavenProject project, boolean transitive) {
+          return Collections.emptyList();
+        }
+
+        @Override
+        public List<MavenProject> getUpstreamProjects(MavenProject project, boolean transitive) {
+          return Collections.emptyList();
+        }
+      });
+      return session;
+    }
+
+    @Override
+    public Mojo lookupConfiguredMojo(MavenSession session, MojoExecution execution) throws Exception {
+      Mojo mojo = super.lookupConfiguredMojo(session, execution);
+      Artifact workspaceResolver = new ArtifactStub();
+      workspaceResolver.setFile(new File("target/workspaceResolver.jar").getCanonicalFile());
+      ReflectionUtils.setVariableValueInObject(mojo, "workspaceResolver", workspaceResolver);
+      return mojo;
+    }
+  };
+
+  @Test
+  public void testIncremental() throws Exception {
+    File basedir = resources.getBasedir("incremental");
+    mojos.executeMojo(basedir, "testProperties");
+    mojos.assertBuildOutputs(basedir, "target/test-classes/test.properties", "target/workspacestate.properties");
+
+    File testProperties = new File(basedir, "target/test-classes/test.properties");
+    File workspaceState = new File(basedir, "target/workspacestate.properties");
+
+    long testPropertiesLastmodified = testProperties.lastModified();
+    long workspaceStateLastmodified = workspaceState.lastModified();
+
+    HashCode testPropertiesSha1 = Files.hash(testProperties, Hashing.sha1());
+    HashCode workspaceStateSha1 = Files.hash(workspaceState, Hashing.sha1());
+
+    mojos.executeMojo(basedir, "testProperties");
+    // mojos.assertCarriedOverOutputs(basedir, "target/test-classes/test.properties", "target/workspacestate.properties");
+
+    Assert.assertEquals(testPropertiesLastmodified, testProperties.lastModified());
+    Assert.assertEquals(workspaceStateLastmodified, workspaceState.lastModified());
+
+    Assert.assertEquals(testPropertiesSha1, sha1(basedir, "target/test-classes/test.properties"));
+    Assert.assertEquals(workspaceStateSha1, sha1(basedir, "target/workspacestate.properties"));
+  }
+
+  private HashCode sha1(File basedir, String path) throws IOException {
+    return Files.hash(new File(basedir, path), Hashing.sha1());
+  }
+}
