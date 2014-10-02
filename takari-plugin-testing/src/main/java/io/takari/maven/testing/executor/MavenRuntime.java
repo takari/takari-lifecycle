ec401310f067f21887bb78d17c8d71071fbb24b4
Jason van Zyl <jason@tesla.io>
2014-10-02 14:59:33 -0400

    The result of running the license-maven-plugin:format to insert headers
    Right now only the application code has had the license headers inserted.
    Not really sure if the test sources requires this, but the transformation
    of the file to insert the header does not respect the original encoding
    of files and causes problem with our tests where encoding is involved.

--- takari-plugin-testing/src/main/java/io/takari/maven/testing/executor/MavenRuntime.java
+++ takari-plugin-testing/src/main/java/io/takari/maven/testing/executor/MavenRuntime.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014 Takari, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package io.takari.maven.testing.executor;
 
 import static org.eclipse.m2e.workspace.WorkspaceState.SYSPROP_STATEFILE_LOCATION;
