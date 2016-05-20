5a7654425c89189815404a2d38230085931c3352
Anton Tanasenko <atg.sleepless@gmail.com>
2016-05-20 14:10:15 +0300

    Remove requiresDependencyResolution from sisu-index mojos (#36)
    Signed-off-by: Anton Tanasenko <atg.sleepless@gmail.com>

--- takari-lifecycle-plugin/src/main/java/io/takari/maven/plugins/sisu/SisuTestIndexMojo.java
+++ takari-lifecycle-plugin/src/main/java/io/takari/maven/plugins/sisu/SisuTestIndexMojo.java
@@ -5,9 +5,8 @@ import java.io.File;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
 
-@Mojo(name = "sisu-test-index", defaultPhase = LifecyclePhase.PROCESS_TEST_CLASSES, threadSafe = true, requiresDependencyResolution = ResolutionScope.TEST)
+@Mojo(name = "sisu-test-index", defaultPhase = LifecyclePhase.PROCESS_TEST_CLASSES, threadSafe = true)
 public class SisuTestIndexMojo extends AbstractSisuIndexMojo {
 
   public static final String PATH_SISU_INDEX = "META-INF/sisu/javax.inject.Named";
