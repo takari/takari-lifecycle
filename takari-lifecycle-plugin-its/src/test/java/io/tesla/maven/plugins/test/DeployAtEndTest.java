ac1c84f931e9351279c9aea5e84341638d68f580
Tamas Cservenak <tamas@cservenak.net>
2025-04-30 23:54:39 +0200

    Hack it around
    But the IT framework should be really supporting Maven 4

--- takari-lifecycle-plugin-its/src/test/java/io/tesla/maven/plugins/test/DeployAtEndTest.java
+++ takari-lifecycle-plugin-its/src/test/java/io/tesla/maven/plugins/test/DeployAtEndTest.java
@@ -2,14 +2,19 @@ package io.tesla.maven.plugins.test;
 
 import io.takari.maven.testing.executor.MavenExecutionResult;
 import io.takari.maven.testing.executor.MavenRuntime.MavenRuntimeBuilder;
+import io.takari.maven.testing.executor.MavenVersions;
 import java.io.File;
 import org.junit.Assert;
 import org.junit.Test;
 
+// TODO: This IT is not running with Maven 4 due following: embedded uses "magic" method, not present in maven 4
+// anymore, so forked is must to be used, but alas, forked throws on (even expected) errors, and does not return result
+// as one would expect.
+@MavenVersions({"3.6.3", "3.8.8", "3.9.9"})
 public class DeployAtEndTest extends AbstractIntegrationTest {
 
     public DeployAtEndTest(MavenRuntimeBuilder verifierBuilder) throws Exception {
-        super(verifierBuilder);
+        super(verifierBuilder, false);
     }
 
     @Test
